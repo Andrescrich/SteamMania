0c9708b9db46c22e9de342b4d7d97ae2d641a9ca
André <andrescrich@hotmail.com>
2019-09-16 00:15:21 +0200

    First

---
+++ SteamMania/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+
+    private Animator _anim;
+    private Rigidbody2D _rb;
+    private SpriteRenderer _sR;
+    private PlayerStates _pS;
+    public bool _canFlip = true;
+    [SerializeField] private float velocity = 350f;
+    [SerializeField] private float jumpForce = 500f;
+
+    private static readonly int Shoot = Animator.StringToHash("Shoot");
+    private static readonly int Speed = Animator.StringToHash("Speed");
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        Physics2D.IgnoreLayerCollision(8, 9);
+        _anim = GetComponentInChildren<Animator>();
+        _rb = GetComponent<Rigidbody2D>();
+        _sR = GetComponentInChildren<SpriteRenderer>();
+        _pS = GetComponent<PlayerStates>();
+
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        
+        
+        
+        var dirMov = Input.GetAxisRaw("Horizontal");
+        Move(dirMov, velocity);
+        
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            _anim.SetTrigger(Shoot);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+            Jump();
+        
+        
+        _anim.SetFloat(Speed, Mathf.Abs(dirMov));
+    }
+
+    private void Move(float dirMov, float vel)
+    {
+        var vector = new Vector2(dirMov * vel * Time.deltaTime, _rb.velocity.y);
+        _rb.velocity = vector;
+        
+        if (!_canFlip) return;
+        if (dirMov > 0)
+        {
+            _sR.flipX = true;
+        }
+        else if (dirMov < 0)
+        {
+            _sR.flipX = false;
+        }
+    }
+
+    private void Jump()
+    {
+        if (!_pS.isGrounded) return;
+        var vector = new Vector2(_rb.velocity.x, jumpForce * Time.deltaTime);
+        _rb.velocity = vector;
+    }
+    
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.layer == 10)
+        {
+            _pS.isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.layer == 10)
+        {
+            _pS.isGrounded = false;
+        }
+    }
+}
---
+++ SteamMania/Assets/ShootingFXScript.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootingFXScript : MonoBehaviour
+{
+    public Transform _pSL;
+    public Transform _pSR;
+    public GameObject particle;
+    public GameObject bullet;
+    private PlayerMovement _pM;
+    private SpriteRenderer _sR;
+
+    private void Awake()
+    {
+        _pM = GetComponentInParent<PlayerMovement>();
+        _sR = GetComponentInParent<SpriteRenderer>();
+    }
+
+    public void PlayParticle()
+    {
+        if (_sR.flipX)
+        {
+            Instantiate(particle, _pSR.transform.position, _pSR.transform.rotation);
+            Instantiate(bullet, _pSR.transform.position, _pSR.transform.rotation);
+        }
+        else
+        {
+            Instantiate(particle, _pSL.transform.position, _pSL.transform.rotation);
+            Instantiate(bullet, _pSL.transform.position, _pSL.transform.rotation);
+        }
+    }
+    
+    public void CanFlip()
+    {
+        _pM._canFlip = !_pM._canFlip;
+    }
+}
